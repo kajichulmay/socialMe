61dbebae32e0a02ce78fcf87b5503539a25f8ce1
chonthicha sangcharean <kajichul.chon@gmail.com>
2021-10-20 12:22:45 +0700

    add headlessui and heroicon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Content from './components/layout/Content';
 import Header from './components/layout/Header';
+import Headlessui from './components/layout/Headlessui';
 import TestTailwind from './components/TestTailwind';
 
 function App() {
   return (
     <>
       <Header />
+      <Headlessui />
       <Content>test2</Content>
     </>
   );
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 function Header({ children }) {
-  return <div className="flex">testtt</div>;
+  return (
+    <div className="flex">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-12 w-12"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+        />
+      </svg>
+    </div>
+  );
 }
 
 export default Header;
