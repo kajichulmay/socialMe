d03e0f51742117901334f2b9244ba2310cc5744a
TeamNutty <nattadolsmash@gmail.com>
2021-10-20 18:33:29 +0700

    login component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
-import TestTailwind from './components/TestTailwind';
+import "./App.css";
+import TestTailwind from "./components/TestTailwind";
+import Login from "./login/Login";
 
 function App() {
-  return (
-    <div className="App">
-      <TestTailwind />
-    </div>
-  );
+    return (
+        <div className="App">
+            {/* <TestTailwind /> */}
+            <Login />
+        </div>
+    );
 }
 
 export default App;
