e01cb2d153bbe9accb040a93931cb33ca5672ab1
chonthicha sangcharean <kajichul.chon@gmail.com>
2021-10-20 18:28:30 +0700

    titleProfile

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import Content from './components/layout/Content';
 import Header from './components/layout/Header';
 import Headlessui from './components/layout/Headlessui';
 import TestTailwind from './components/TestTailwind';
-
+import MyProfile from './pages/MyProfile';
+import { Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <>
       <Header />
-      <Headlessui />
-      <Content>test2</Content>
+      <Content>
+        <Switch>
+          <Route exact path="/myprofile" component={MyProfile} />
+        </Switch>
+      </Content>
     </>
   );
 }
--- src/components/layout/Content.js
+++ src/components/layout/Content.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Content({ children }) {
-  return <div className=" md:container md:mx-auto flex bg-green-400">{children}</div>;
+  return <div className=" md:container md:mx-auto flex">{children}</div>;
 }
 
 export default Content;
