a1c86a4c008b055866dd0d5ad9fa99834af43d64
TeamNutty <nattadolsmash@gmail.com>
2021-10-21 18:35:41 +0700

    chatbox

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,99 +1,93 @@
-import React, { useState } from 'react';
-import logoHeader from '../../images/logoHeader.png';
-import tempProfileHeader from '../../mockData/images/tempProfileHeader.png';
-import Toggle from './Toggle';
+import React, { useState } from "react";
+import logoHeader from "../../images/logoHeader.png";
+import tempProfileHeader from "../../mockData/images/tempProfileHeader.png";
+import DropdownNotification from "../dropdown/DropdownNotification";
+import Toggle from "./Toggle";
 
 function Header({ children }) {
-  const [enabled, setEnabled] = useState(false);
+    const [enabled, setEnabled] = useState(false);
 
-  const gd = (
-    <defs>
-      <linearGradient id="iconGrad" x2="0%" y2="100%">
-        <stop offset="0%" stop-color="rgba(255, 233, 155, 1)" />
-        <stop offset="40%" stop-color="rgba(255, 171, 124, 1)" />
-        <stop offset="100%" stop-color="rgba(255, 86, 80, 1)" />
-      </linearGradient>
-    </defs>
-  );
+    const gd = (
+        <defs>
+            <linearGradient id="iconGrad" x2="0%" y2="100%">
+                <stop offset="0%" stop-color="rgba(255, 233, 155, 1)" />
+                <stop offset="40%" stop-color="rgba(255, 171, 124, 1)" />
+                <stop offset="100%" stop-color="rgba(255, 86, 80, 1)" />
+            </linearGradient>
+        </defs>
+    );
 
-  return (
-    <div className="flex bg-white items-center justify-between h-16 p-1.5 shadow-container header-border fixed top-0 w-full z-20 rounded-b-3xl">
-      <div className="flex items-center  h-full">
-        <img className="w-10 h-10 mx-3" src={logoHeader} alt="" />
+    return (
+        <div className="flex bg-white items-center justify-between h-16 p-1.5 shadow-container header-border fixed top-0 w-full z-20 rounded-b-3xl">
+            <div className="flex items-center  h-full">
+                <img className="w-10 h-10 mx-3" src={logoHeader} alt="" />
 
-        <div className="bg-primary-grad h-11 flex items-center w-auto p-0.5 rounded-full mx-6 shadow-md ">
-          <div className="flex items-center rounded-full  bg-white h-full  ">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4 mx-5"
-              fill="#fff"
-              viewBox="0 0 24 24"
-              stroke="#FF4949"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
+                <div className="bg-primary-grad h-11 flex items-center w-auto p-0.5 rounded-full mx-6 shadow-md ">
+                    <div className="flex items-center rounded-full  bg-white h-full  ">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-4 w-4 mx-5"
+                            fill="#fff"
+                            viewBox="0 0 24 24"
+                            stroke="#FF4949"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                            />
+                        </svg>
 
-            <input
-              className=" w-60 text-base  rounded-full outline-none h-full "
-              type="text"
-              placeholder="search people"
-            />
-          </div>
+                        <input
+                            className=" w-60 text-base  rounded-full outline-none h-full "
+                            type="text"
+                            placeholder="search people"
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className="flex items-center">
+                <div className="text-base text-dark font-normal flex-shrink">Welcome Guest</div>
+                <div className="shadow-input rounded-full mx-3">
+                    <img class="rounded-full h-9 w-9" src={tempProfileHeader} alt="" />
+                </div>
+
+                <div className=" flex items-center justify-center bg-white rounded-full h-9 w-9 shadow-input border border-red-300">
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 icon-grad" viewBox="0 0 20 20">
+                            {gd}
+                            <path
+                                fillRule="evenodd"
+                                d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
+                    </div>
+                </div>
+                <DropdownNotification />
+                <Toggle />
+
+                <div className="flex items-center justify-center bg-white rounded-full h-9 w-9 shadow-input ml-3 border border-red-300 icon-grad">
+                    <div>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="h-5 w-5 icon-grad"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                        >
+                            {gd}
+                            <path
+                                fill-rule="evenodd"
+                                d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
+                                clip-rule="evenodd"
+                            />
+                        </svg>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-      <div className="flex items-center">
-        <div className="text-base text-dark font-normal flex-shrink">Welcome Guest</div>
-        <div className="shadow-input rounded-full mx-3">
-          <img class="rounded-full h-9 w-9" src={tempProfileHeader} alt="" />
-        </div>
-
-        <div className=" flex items-center justify-center bg-white rounded-full h-9 w-9 shadow-input border border-red-300">
-          <div>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 icon-grad" viewBox="0 0 20 20">
-              {gd}
-              <path
-                fillRule="evenodd"
-                d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </div>
-        </div>
-        <div className=" flex items-center justify-center bg-white rounded-full h-9 w-9 shadow-input ml-3 border border-red-300">
-          <div>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 icon-grad" viewBox="0 0 24 24">
-              {gd}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-              />
-            </svg>
-          </div>
-        </div>
-        <Toggle />
-
-        <div className="flex items-center justify-center bg-white rounded-full h-9 w-9 shadow-input ml-3 border border-red-300 icon-grad">
-          <div>
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 icon-grad" viewBox="0 0 20 20" fill="currentColor">
-              {gd}
-              <path
-                fill-rule="evenodd"
-                d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Header;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,85 +1,114 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import { useHistory } from "react-router-dom";
 
 function Register() {
-  // history
-  const history = useHistory();
+    // history
+    const history = useHistory();
 
-  // function
-  const handleclickSignin = () => {
-    history.push('/');
-  };
+    // function
+    const handleclickSignin = () => {
+        history.push("/");
+    };
 
-  return (
-    <div className="w-full justify-center mt-10 flex lg:h-full lg:mt-20">
-      <div className="shadow-lg w-full flex flex-col bg-white md:w-2/3 lg:flex-row lg:h-5/6 lg:rounded-3xl">
-        <div className="bg-primary-grad text-white w-full p-10 pt-20 flex flex-col  items-center lg:w-2/3 lg:rounded-l-3xl lg:pt-32">
-          <p className="text-4xl">Register Now!!!</p>
-          <div className="flex flex-col justify-between items-center h-1/3 mt-10 font-light lg:h-1/3 lg:justify-between">
-            <p className="text-xl">Create Your new account</p>
-            <p className="text-xl">To Create youe comunity</p>
-            <p className="text-xl">Join your friend</p>
-            <p className="text-xl">and show Who you are</p>
-          </div>
-          <button
-            onClick={handleclickSignin}
-            className="h-12 bg-white text-red-400 shadow-lg rounded-full p-2 w-1/2 mt-20"
-          >
-            SIGN IN
-          </button>
+    return (
+        <div className="w-full justify-center mt-10 flex lg:h-full lg:mt-20">
+            <div className="shadow-lg w-full flex flex-col bg-white md:w-2/3 lg:flex-row lg:h-5/6 lg:rounded-3xl">
+                <div className="bg-primary-grad text-white w-full p-10 pt-20 flex flex-col  items-center lg:w-2/3 lg:rounded-l-3xl lg:pt-32">
+                    <p className="text-4xl">Register Now!!!</p>
+                    <div className="flex flex-col justify-between items-center h-1/3 mt-10 font-light lg:h-1/3 lg:justify-between">
+                        <p className="text-xl">Create Your new account</p>
+                        <p className="text-xl">To Create youe comunity</p>
+                        <p className="text-xl">Join your friend</p>
+                        <p className="text-xl">and show Who you are</p>
+                    </div>
+                    <button
+                        onClick={handleclickSignin}
+                        className="h-12 bg-white text-red-400 shadow-lg rounded-full p-2 w-1/2 mt-20"
+                    >
+                        SIGN IN
+                    </button>
+                </div>
+                <div className="p-10 flex flex-col justify-between items-center w-full pt-20 lg:h-full">
+                    <p className="text-gray-400 text-2xl">Register new account</p>
+                    <form className="flex flex-col h-full justify-between w-2/3 mt-10 lg:h-3/5">
+                        {/* form */}
+                        <div className=" mx-3">
+                            <div class="relative">
+                                <input
+                                    type="text"
+                                    className="w-full h-12 border rounded-full border-red-400 p-1.5 mt-5 shadow-lg pl-3 focus:outline-none focus:ring-2 focus:ring-red-400"
+                                />
+                                <div class="absolute top-2 p-1 bg-white left-4 ">
+                                    <p className="text-red-600 text-dark text-sm font-normal">Firstname</p>
+                                </div>
+                            </div>
+                        </div>
+                        <p className="pl-10 text-red-600 mt-2 mb-5">Firstname is required</p>
+
+                        {/* form */}
+                        <div className=" mx-3">
+                            <div class="relative">
+                                <input
+                                    type="text"
+                                    className="w-full h-12 border rounded-full border-red-400 p-1.5 mt-5 shadow-lg pl-3 focus:outline-none focus:ring-2 focus:ring-red-400"
+                                />
+                                <div class="absolute top-2 p-1 bg-white left-4 ">
+                                    <p className="text-red-600 text-dark text-sm font-normal">Lastname</p>
+                                </div>
+                            </div>
+                        </div>
+                        <p className="pl-10 text-red-600 mt-2 mb-5">Lastname is required</p>
+
+                        {/* form */}
+                        <div className=" mx-3">
+                            <div class="relative">
+                                <input
+                                    type="text"
+                                    className="w-full h-12 border rounded-full border-red-400 p-1.5 mt-5 shadow-lg pl-3 focus:outline-none focus:ring-2 focus:ring-red-400"
+                                />
+                                <div class="absolute top-2 p-1 bg-white left-4 ">
+                                    <p className="text-red-600 text-dark text-sm font-normal">Email Address</p>
+                                </div>
+                            </div>
+                        </div>
+                        <p className="pl-10 text-red-600 mt-2 mb-5">Email is required</p>
+
+                        {/* form */}
+                        <div className=" mx-3">
+                            <div class="relative">
+                                <input
+                                    type="password"
+                                    className="w-full h-12 border rounded-full border-red-400 p-1.5 mt-5 shadow-lg pl-3 focus:outline-none focus:ring-2 focus:ring-red-400"
+                                />
+                                <div class="absolute top-2 p-1 bg-white left-4 ">
+                                    <p className="text-red-600 text-dark text-sm font-normal">Password</p>
+                                </div>
+                            </div>
+                        </div>
+                        <p className="pl-10 text-red-600 mt-2 mb-5">Password is required</p>
+
+                        {/* form */}
+                        <div className=" mx-3">
+                            <div class="relative">
+                                <input
+                                    type="password"
+                                    className="w-full h-12 border rounded-full border-red-400 p-1.5 mt-5 shadow-lg pl-3 focus:outline-none focus:ring-2 focus:ring-red-400"
+                                />
+                                <div class="absolute top-2 p-1 bg-white left-4 ">
+                                    <p className="text-red-600 text-dark text-sm font-normal">Confirm Password</p>
+                                </div>
+                            </div>
+                        </div>
+                        <p className="pl-10 text-red-600 mt-2 mb-5">Confirm Password is required</p>
+
+                        <button className="h-12 bg-primary-grad forhover text-white rounded-full p-2 shadow-lg mt-5 w-3/4 m-auto lg:w-1/2">
+                            Register
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
-        <div className="p-10 flex flex-col justify-between items-center w-full pt-20 lg:h-full">
-          <p className="text-gray-400 text-2xl">Register new account</p>
-          <form className="flex flex-col h-full justify-between w-2/3 mt-10 lg:h-3/5">
-            {/* form */}
-            <input
-              type="text"
-              className="h-12 border rounded-full border-red-400 p-1.5 shadow-lg pl-5 focus:outline-none focus:ring-2 focus:ring-red-400"
-              placeholder="Firstname"
-            />
-            <p className="pl-10 text-red-600 mt-2 mb-5">Firstname is required</p>
-            {/* icon */}
-
-            {/* form */}
-            <input
-              type="text"
-              className="h-12 border rounded-full border-red-400 p-1.5 shadow-lg pl-5 focus:outline-none focus:ring-2 focus:ring-red-400"
-              placeholder="Lastname"
-            />
-            <p className="pl-10 text-red-600 mt-2 mb-5">Lastname is required</p>
-
-            {/* form */}
-            <input
-              type="text"
-              className="h-12 border rounded-full border-red-400 p-1.5 shadow-lg pl-5 focus:outline-none focus:ring-2 focus:ring-red-400"
-              placeholder="Email Address"
-            />
-            <p className="pl-10 text-red-600 mt-2 mb-5">Email is required</p>
-
-            {/* form */}
-            <input
-              type="password"
-              className="h-12 border rounded-full border-red-400 p-1.5 shadow-lg pl-5 focus:outline-none focus:ring-2 focus:ring-red-400"
-              placeholder="Password"
-            />
-            <p className="pl-10 text-red-600 mt-2 mb-5">Password is required</p>
-
-            {/* form */}
-            <input
-              type="password"
-              className="h-12 border rounded-full border-red-400 p-1.5 shadow-lg pl-5 focus:outline-none focus:ring-2 focus:ring-red-400"
-              placeholder="Confirm Password"
-            />
-            <p className="pl-10 text-red-600 mt-2 mb-5">Confirm Password is required</p>
-
-            <button className="h-12 bg-primary-grad forhover text-white rounded-full p-2 shadow-lg mt-5 w-3/4 m-auto lg:w-1/2">
-              Register
-            </button>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Register;
