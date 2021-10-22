4aee1c38d8fb8e2502c19ec6d36a589ba38238c4
sayakass <sayaka.jp@icloud.com>
2021-10-22 15:09:32 +0700

    profile setting

--- src/components/profileSetting/ProfileSetting.js
+++ src/components/profileSetting/ProfileSetting.js
@@ -5,7 +5,7 @@ import addRound from '../../images/Add_round.png';
 
 export default function ProfileSetting() {
   return (
-    <div className="w-full px-4 pt-16">
+    <div className="w-full px-4 pt-16 bg-secondary mt-10">
       <div className=" max-w-xl p-5 mx-auto bg-white border-50">
         <div className="flex items-center">
           <div className="w-36 h-36 rounded-full">
@@ -78,37 +78,75 @@ export default function ProfileSetting() {
               </div>
             </div>
           </div>
-          <div className="inputFollwer w-1/4 flex-shrink justify-center px-3">
-            <button className="flex-shrink rounded-full shadow-input w-32 h-8 bg-primary-grad text-white font-thin forhover">
+          <div className="inputFollwer w-full flex-shrink  px-3 right relative">
+            <button className="flex-shrink rounded-full shadow-input w-32 h-8 bg-primary-grad text-white font-thin forhover mt-5 object-right right-5 absolute">
               Save change
             </button>
           </div>
         </form>
-        <Disclosure>
-          {({ open }) => (
-            <>
-              <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-purple-900 bg-purple-100 rounded-lg hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-                <span>What is your refund policy?</span>
-                <ChevronUpIcon className={`${open ? 'transform rotate-180' : ''} w-5 h-5 text-purple-500`} />
-              </Disclosure.Button>
-              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                If you're unhappy with your purchase for any reason, email us within 90 days and we'll refund you in
-                full, no questions asked.
-              </Disclosure.Panel>
-            </>
-          )}
-        </Disclosure>
-        <Disclosure as="div" className="mt-2">
-          {({ open }) => (
-            <>
-              <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-purple-900 bg-purple-100 rounded-lg hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-                <span>Do you offer technical support?</span>
-                <ChevronUpIcon className={`${open ? 'transform rotate-180' : ''} w-5 h-5 text-purple-500`} />
-              </Disclosure.Button>
-              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">No.</Disclosure.Panel>
-            </>
-          )}
-        </Disclosure>
+        <form>
+          <div className="mt-5">
+            <Disclosure>
+              {({ open }) => (
+                <>
+                  <Disclosure.Button className="flex justify-between w-auto px-4 py-2 text-xs text-left text-dark-thin font-thin ">
+                    <ChevronUpIcon className={`${open ? 'transform rotate-180' : ''} w-5 h-5 text-red-400`} />
+                    <span>Change password ?</span>
+                  </Disclosure.Button>
+                  <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                    <form className="mt-20 mb-10 ">
+                      <div className=" w-1/3">Reset your password</div>
+                      <div className="w-full flex ">
+                        <div className="w-1/2">
+                          <div className="mx-3 mt-5">
+                            <div class="relative">
+                              <input
+                                type="text"
+                                className="w-full h-10 border rounded-full border-red-400 p-1.5 mt-5 shadow-lg pl-3 focus:outline-none focus:ring-2 focus:ring-red-400"
+                              />
+                              <div class="absolute top-2 p-1 bg-white left-4 ">
+                                <p className="text-red-600 text-dark text-xs font-normal">New password</p>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="mx-3 mt-3">
+                            <div class="relative">
+                              <input
+                                type="text"
+                                className="w-full h-10 border rounded-full border-red-400 p-1.5 mt-5 shadow-lg pl-3 focus:outline-none focus:ring-2 focus:ring-red-400"
+                              />
+                              <div class="absolute top-2 p-1 bg-white left-4 ">
+                                <p className="text-red-600 text-dark text-xs font-normal">Confirm new password</p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="w-1/2">
+                          <div className="mx-3 mt-5">
+                            <div class="relative">
+                              <input
+                                type="text"
+                                className="w-full h-10 border rounded-full border-red-400 p-1.5 mt-5 shadow-lg pl-3 focus:outline-none focus:ring-2 focus:ring-red-400"
+                              />
+                              <div class="absolute top-2 p-1 bg-white left-4 ">
+                                <p className="text-red-600 text-dark text-xs font-normal">Current password</p>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="inputFollwer w-full flex-shrink  px-3 right relative mt-4 mb-20">
+                            <button className="flex-shrink rounded-full shadow-input w-32 h-8 bg-primary-grad text-white font-thin forhover mt-5 object-right right-5 absolute">
+                              Reset password
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </form>
+                  </Disclosure.Panel>
+                </>
+              )}
+            </Disclosure>
+          </div>
+        </form>
       </div>
     </div>
   );
