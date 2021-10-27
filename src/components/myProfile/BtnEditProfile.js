d44b0378453492108c4376317f7678309aac5453
sayakass <sayaka.jp@icloud.com>
2021-10-27 11:42:39 +0700

    merge with earth

--- src/components/myProfile/BtnEditProfile.js
+++ src/components/myProfile/BtnEditProfile.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import { useHistory } from 'react-router-dom';
 
 function BtnEditProfile() {
@@ -9,38 +8,15 @@ function BtnEditProfile() {
   };
 
   return (
-    <div className="ml-5 text-black rounded-full ">
-      <button
-        onClick={handleClickEditProfile}
-        className={`rounded-full shadow-input px-3 py-2 flex justify-center items-center 
+    <button
+      onClick={handleClickEditProfile}
+      className={`bg-white rounded-full shadow-input px-3 py-2
+        flex justify-center items-center 
 
         `}
-      >
-        Edit Profile
-      </button>
-    </div>
-=======
-import { NavLink } from 'react-router-dom';
-import settingIcon from '../../images/settingIcon.png';
-
-function BtnEditProfile() {
-
-
-
-  return (
-
-    <NavLink to="/profile-setting">
-      <button
-        className="bg-white rounded-full shadow-input px-3 py-2
-        flex justify-center items-center
-        "
-      >
-        Edit Profile
-        <img src={settingIcon} alt="setting-profile-icon" className="ml-1" />
-      </button>
-    </NavLink>
-
->>>>>>> 95a1fc4dbfcc3106a62195a838ce8d37b619ffa6
+    >
+      Edit Profile
+    </button>
   );
 }
 
