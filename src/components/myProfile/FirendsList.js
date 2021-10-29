2822ce03141aff14014bc5c21067edb65e4dad71
sayakass <sayaka.jp@icloud.com>
2021-10-29 11:22:39 +0700

    first commit

--- src/components/myProfile/FirendsList.js
+++ src/components/myProfile/FirendsList.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ProfilePicUi from '../ui/ProfilePicUi';
 import mockFriendsChat from './../../mockData/image/mockFriendsChat.png';
+import { DarkContext } from '../../context/DarkContext';
+
 function FirendsList({ firstName, lastName, profilePicture, idFriend }) {
+  const { dark } = useContext(DarkContext);
   return (
     <div
-      className="my-2 w-10/12 flex
-    items-center cursor-pointer  mb-2"
+      className={`${dark ? 'text-white' : ''} my-2 w-10/12 flex
+    items-center cursor-pointer  mb-2`}
     >
       <ProfilePicUi beforeSize="16" afterSize="16" url={profilePicture} id={idFriend} />
       {/* <img src={mockFriendsChat} className="mr-5" /> */}
